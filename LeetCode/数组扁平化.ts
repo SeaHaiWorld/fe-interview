750ebd0d63558746e0cf31deb6a9ff1d305df317
yang <790041427@qq.com>
2024-05-24 11:32:31 +0800

    code: 数组扁平化2

--- LeetCode/数组扁平化.ts
+++ LeetCode/数组扁平化.ts
@@ -1,16 +1,36 @@
-function flattenAndSort(arr) {
-    const flatArray = arr.reduce((acc, curr) => {
-        return acc.concat(Array.isArray(curr) ? flattenAndSort(curr) : curr);
+// function flattenAndSort(arr) {
+//     const flatArray = arr.reduce((acc, curr) => {
+//         return acc.concat(Array.isArray(curr) ? flattenAndSort(curr) : curr);
+//     }, []);
+
+//     const uniqueArray = [...new Set(flatArray)] as number[];
+
+//     uniqueArray.sort((a, b) => a - b);
+
+//     return uniqueArray;
+// }
+
+// const arr = [ [1, 2, 2], [3, 4, 5, 5], [6, 7, 8, 9, [11, 12, [12, 13, [14]]]], 10];
+
+// const result = flattenAndSort(arr);
+// console.log(result);
+
+function flatArray(arr) {
+    // 扁平化数组
+    const flattened = arr.reduce((acc, cur) => {
+        return acc.concat(Array.isArray(cur) ? flatArray(cur) : cur);
     }, []);
 
-    const uniqueArray = [...new Set(flatArray)] as number[];
+    // 去除重复元素
+    const uniqueArray = [...new Set(flattened)];
 
-    uniqueArray.sort((a, b) => a - b);
+    // 排序
+    const sortedArray = uniqueArray.sort((a, b) => a - b);
 
-    return uniqueArray;
+    return sortedArray;
 }
 
-const arr = [ [1, 2, 2], [3, 4, 5, 5], [6, 7, 8, 9, [11, 12, [12, 13, [14]]]], 10];
+const arr = [[1, 2, 2], [3, 4, 5, 5], [6, 7, 8, 9, [11, 12, [12, 13, [14]]]], 10];
 
-const result = flattenAndSort(arr);
+const result = flatArray(arr);
 console.log(result);
