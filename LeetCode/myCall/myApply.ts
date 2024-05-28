d5c08cf7562b0b2407f68e461e3ff62de2a79738
yang <790041427@qq.com>
2024-05-28 19:41:36 +0800

    code: 实现call或apply

---
+++ LeetCode/myCall/myApply.ts
@@ -0,0 +1,34 @@
+// 手写实现 call 方法
+interface Function {
+  myCall(context: any, ...args: any[]): any
+  myApply(context: any, argsArr: any[]): any
+}
+
+Function.prototype.myCall = function (context: any, ...args: any[]): any {
+    context = context || window
+    let uniqueKey = Symbol()
+
+    context[uniqueKey] = this;
+
+    const result = context[uniqueKey](...args);
+
+    // 删除临时方法
+    delete context[uniqueKey];
+
+    return result;
+}
+
+Function.prototype.myApply = function (context: any, ...args: any[]): any {
+    context = context || window
+    let uniqueKey = Symbol()
+
+    context[uniqueKey] = this;
+
+    const result = context[uniqueKey](...args);
+
+    // 删除临时方法
+    delete context[uniqueKey];
+
+    return result;
+}
+
