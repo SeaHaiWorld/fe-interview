df03c9e007b33634fcaefff3c815d169a157858c
yang <790041427@qq.com>
2024-05-30 19:52:45 +0800

    code: compose补充序号

--- LeetCode/39.全排列.ts
+++ LeetCode/39.全排列.ts
@@ -18,20 +18,24 @@ function permute(nums: number[]) {
 }
 
 
-function permuteArr(nums: number[]) {
-    let res: number[][] = []
+function permuteArr(nums: number[]): number[][] {
+    let res: number[][] = [];
 
     function backTrack(arr: number[], remain: number[]) {
         if (remain.length === 0) {
-            res.push([...arr])
+            res.push([...arr]); // 使用副本
+            return;
         }
 
         for (let i = 0; i < remain.length; i++) {
-            let newRemaining = [...remain.slice(0, i), ...remain.slice(i+1)]
-            backTrack(arr.concat(remain[i]), newRemaining)
+            let newRemaining = [...remain.slice(0, i), ...remain.slice(i + 1)];
+            backTrack(arr.concat(remain[i]), newRemaining);
         }
     }
 
-    backTrack([], nums)
-    return res
+    backTrack([], nums);
+    return res;
 }
+
+const nums = [1, 2, 3];
+console.log(permuteArr(nums)); // 输出全排列
